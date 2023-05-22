2c5a5b6de39bdfcf110deff658b553c2eee13fad
kushal <kushalbhetuwal@gmail.com>
2023-05-22 10:09:41 +1000

    added

---
+++ backend/data.js
@@ -0,0 +1,236 @@
+const data = {
+    products:[ {
+        name:"Milk",
+        slug:"freshmilk", 
+        image:'/images/dairy1.jpg',
+        price:2, 
+        countInStock: 10, 
+        brand:"harris", 
+        healthrating:5, 
+        reviews:10, 
+        description:"healthy_fresh_milk"
+    }, 
+    {
+        name:"carrot",
+        slug:"fresh-carrot", 
+        image:'/images/download6.jpg',
+        price:3,
+        countInStock: 12, 
+        brand:"harris", 
+        healthrating:5, 
+        reviews:10, 
+        description:"healthy_fresh_carrot"
+    }, 
+    {
+    name:"apple",
+    slug:"fresh_apple", 
+    image:'/images/fruits1.jpg',
+    price:9.8, 
+    countInStock: 14, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_apple"
+}, 
+{
+    name:"zuchini",
+    slug:"fresh_Zuchini", 
+    image:'/images/download1.jpg',
+    price:2.7, 
+    countInStock: 15, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Yoghurt",
+    slug:"fresh_Yoghurt", 
+    image:'/images/dairy2.jpg',
+    price:10, 
+    countInStock: 20, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Yoghurt"
+},
+{
+    name:"ghee",
+    slug:"fresh_Zuchini", 
+    image:'/images/dairy3.jpg',
+    price:9.5, 
+    countInStock: 40, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"cheese",
+    slug:"fresh_Zuchini", 
+    image:'/images/dairy4.jpg',
+    price:2.5, 
+    countInStock: 50, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"tomato",
+    slug:"fresh_Zuchini", 
+    image:'/images/download.jpg',
+    price:4.5, 
+    countInStock: 40, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"lettuce",
+    slug:"fresh_Zuchini", 
+    image:'/images/download2.jpg',
+    price:8.5, 
+    countInStock: 10, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Spinach",
+    slug:"fresh_Zuchini", 
+    image:'/images/download3.jpg',
+    price:7.5, 
+    countInStock: 10, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Cucumber",
+    slug:"fresh_Zuchini", 
+    image:'/images/download4.jpg',
+    price:6, 
+    countInStock: 20, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Brocoli",
+    slug:"fresh_Zuchini", 
+    image:'/images/download5.jpg',
+    price:3, 
+    countInStock: 20, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Carrot",
+    slug:"fresh_Zuchini", 
+    image:'/images/download6.jpg',
+    price:4, 
+    countInStock: 50, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Potato",
+    slug:"fresh_Zuchini", 
+    image:'/images/download7.jpg',
+    price:5, 
+    countInStock: 110, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Salad",
+    slug:"fresh_Zuchini", 
+    image:'/images/download8.jpg',
+    price:5, 
+    countInStock: 110, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Ginger",
+    slug:"fresh_Zuchini", 
+    image:'/images/download9.jpg',
+    price:9, 
+    countInStock: 120, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Onions",
+    slug:"fresh_Zuchini", 
+    image:'/images/download10.jpg',
+    price:8, 
+    countInStock: 130, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Oranges",
+    slug:"fresh_Zuchini", 
+    image:'/images/fruits2.jpg',
+    price:7, 
+    countInStock: 140, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Persimmon",
+    slug:"fresh_Zuchini", 
+    image:'/images/fruits3.jpg',
+    price:2, 
+    countInStock: 150, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Mango",
+    slug:"fresh_Zuchini", 
+    image:'/images/fruits4.jpg',
+    price:3, 
+    countInStock: 170, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+},
+{
+    name:"Banana",
+    slug:"fresh_Zuchini", 
+    image:'/images/fruits5.jpg',
+    price:4, 
+    countInStock: 150, 
+    brand:"harris", 
+    healthrating:5, 
+    reviews:10, 
+    description:"healthy_fresh_Zuchini"
+}
+
+]}; 
+
+export default data;
